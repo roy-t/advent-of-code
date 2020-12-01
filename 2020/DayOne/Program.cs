36ecaeb6173e7670a331ceae21a96ef3777e4d82
Roy Triesscheijn <roy-t@hotmail.com>
2020-12-01 22:02:42 +0100

    Day One - Part One

---
+++ 2020/DayOne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+var numbers = Input.Numbers
+    .Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(s => int.Parse(s));
+
+var result = numbers.SelectMany(_ => numbers, (l, r) => new { Sum = l + r, Left = l, Right = r })
+    .Where(triple => triple.Sum == 2020)
+    .Select(triple => triple.Left * triple.Right)
+    .First();
+
+Console.WriteLine(result);
+Console.ReadLine();
