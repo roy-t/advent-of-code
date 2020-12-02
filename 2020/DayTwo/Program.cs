a909d0b2e926b045c14c262aa1e430f80b489bc2
Roy Triesscheijn <roy-t@hotmail.com>
2020-12-02 20:54:01 +0100

    Day Two - Part One & Two

---
+++ 2020/DayTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using MoreLinq;
+
+// Part One
+var valid = Input.Passwords
+    .Split(new char[] { '-', ' ', ':', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+    .Batch(4, e => e.ToArray())
+    .Select(batch => new { Min = int.Parse(batch[0]), Max = int.Parse(batch[1]), Letter = batch[2][0], Password = batch[3] })
+    .Where(p => p.Password.Count(c => c == p.Letter) >= p.Min)
+    .Where(p => p.Password.Count(c => c == p.Letter) <= p.Max)
+    .Count();
+
+Console.WriteLine(valid);
+
+// Part Two
+valid = Input.Passwords
+    .Split(new char[] { '-', ' ', ':', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+    .Batch(4, e => e.ToArray())
+    .Select(batch => new { Min = int.Parse(batch[0]), Max = int.Parse(batch[1]), Letter = batch[2][0], Password = batch[3] })
+    .Where(p => p.Password[p.Min - 1] == p.Letter ^ p.Password[p.Max - 1] == p.Letter)
+    .Count();
+
+Console.WriteLine(valid);
+Console.ReadLine();
