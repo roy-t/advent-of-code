6c6d44391c875224fe7d364eeb3ceb2208cd1acc
Roy Triesscheijn <roy-t@hotmail.com>
2020-12-06 18:12:02 +0100

    Day Six - Part One & Two

---
+++ 2020/DaySix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+// Part One
+var result = Input.Groups.Split(new[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(answers =>
+        answers.ToCharArray()
+        .Where(c => char.IsLetter(c))
+        .Distinct()
+        .Count())
+    .Sum();
+
+Console.WriteLine(result);
+
+// Part Two
+result = Input.Groups.Split(new[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(answers =>
+        answers.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(answer => answer.ToCharArray().ToHashSet())
+        .Aggregate((a, b) => { a.IntersectWith(b); return a; })
+        .Count())
+    .Sum();
+
+Console.WriteLine(result);
+
+Console.ReadLine();
